1be0caa24d29ffd4bc68dfb17cc1f13681cdc37a
ethiqque <danielpyld.work@gmail.com>
2024-03-19 19:19:17 +0200

    initial commit

---
+++ src/main/java/com/example/twitter/controller/RegistrationController.java
@@ -0,0 +1,42 @@
+package com.example.twitter.controller;
+
+import com.example.twitter.domain.Message;
+import com.example.twitter.domain.Role;
+import com.example.twitter.repos.MessageRepo;
+import com.example.twitter.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.example.twitter.domain.User;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Controller
+public class RegistrationController {
+    @Autowired
+    private UserRepo userRepo;
+
+    @GetMapping("/registration")
+    public String registration(){
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(User user, Map<String, Object> model){
+        User userFromDb = userRepo.findByUsername(user.getUsername());
+
+        if(userFromDb != null){
+            model.put("message", "User exists!");
+            return "registration";
+        }
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        userRepo.save(user);
+
+        return "redirect:/login";
+    }
+}
